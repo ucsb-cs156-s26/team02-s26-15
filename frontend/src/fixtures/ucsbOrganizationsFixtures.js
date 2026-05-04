6c4a2e60eab79452dca92e4a40907744e9629af4
Joy Cheng <joycheng@ucsb.edu>
2026-05-04 16:06:57 -0700

    joy - create fixture for organizations table

---
+++ frontend/src/fixtures/ucsbOrganizationsFixtures.js
@@ -0,0 +1,30 @@
+const ucsbOrganizationsFixtures = {
+  oneOrganization: {
+    "orgCode": "AACF",
+      "orgTranslationShort": "Asian American Christian Fellowship",
+      "orgTranslation": "UCSB Asian American Christian Fellowship",
+      "inactive": false
+  },
+  threeOrganizations: [
+    {
+      "orgCode": "AACF",
+      "orgTranslationShort": "Asian American Christian Fellowship",
+      "orgTranslation": "UCSB Asian American Christian Fellowship",
+      "inactive": false
+    },
+    {
+      "orgCode": "EPIC",
+      "orgTranslationShort": "Epic UCSB",
+      "orgTranslation": "Epic at Cru UCSB",
+      "inactive": true
+    },
+    {
+      "orgCode": "A2F",
+      "orgTranslationShort": "Acts2Fellowship",
+      "orgTranslation": "UCSB Acts2Fellowship",
+      "inactive": true
+    }
+  ]
+};
+
+export { ucsbOrganizationsFixtures };
