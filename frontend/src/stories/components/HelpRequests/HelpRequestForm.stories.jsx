a1865cbec822b6f308cb8c7f60b37b455bd80241
Kristopher Rangel-Olivos <kristopherrangel@ucsb.edu>
2026-05-04 22:54:28 -0700

    KR - fix formatting

--- frontend/src/stories/components/HelpRequests/HelpRequestForm.stories.jsx
+++ frontend/src/stories/components/HelpRequests/HelpRequestForm.stories.jsx
@@ -24,7 +24,8 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-  initialContents: helpRequestsFixtures.oneHelpRequest,  buttonLabel: "Update",
+  initialContents: helpRequestsFixtures.oneHelpRequest,
+  buttonLabel: "Update",
   submitAction: (data) => {
     console.log("Submit was clicked with data: ", data);
     window.alert("Submit was clicked with data: " + JSON.stringify(data));
