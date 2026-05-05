c355d0654b620c590c1656eb09b316acb5e88a9f
Kristopher Rangel-Olivos <kristopherrangel@ucsb.edu>
2026-05-05 12:26:45 -0700

    KR - switched to UCSBDates form style for handling intial values, to not have mutation errors hopeuflly

--- frontend/src/main/components/HelpRequests/HelpRequestForm.jsx
+++ frontend/src/main/components/HelpRequests/HelpRequestForm.jsx
@@ -8,18 +8,23 @@ function HelpRequestForm({
   buttonLabel = "Create",
 }) {
   // Stryker disable all
+  // const {
+  //   register,
+  //   formState: { errors },
+  //   handleSubmit,
+  // } = useForm({
+  //   defaultValues: initialContents
+  //     ? {
+  //         ...initialContents,
+  //         requestTime: initialContents.requestTime.replace("Z", ""),
+  //       }
+  //     : {},
+  // });
   const {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm({
-    defaultValues: initialContents
-      ? {
-          ...initialContents,
-          requestTime: initialContents.requestTime.replace("Z", ""),
-        }
-      : {},
-  });
+  } = useForm({ defaultValues: initialContents || {} });
   // Stryker restore all
 
   const navigate = useNavigate();
