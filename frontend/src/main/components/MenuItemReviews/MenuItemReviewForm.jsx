8dd725d582394e3dcff5bc2d5454235f58026a30
katelyn-dang <katelyndang@ucsb.edu>
2026-05-04 23:23:16 -0700

    kd - WIP fix formatting

--- frontend/src/main/components/MenuItemReviews/MenuItemReviewForm.jsx
+++ frontend/src/main/components/MenuItemReviews/MenuItemReviewForm.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router";
 function MenuItemReviewForm({
   initialContents,
   submitAction,
-  buttonLabel = "Create"
+  buttonLabel = "Create",
 }) {
   // Stryker disable all
   const {
