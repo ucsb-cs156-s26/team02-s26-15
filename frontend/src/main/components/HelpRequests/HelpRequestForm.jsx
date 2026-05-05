4db26850b99084e43e10b905d031d05a6973ae0c
Kristopher Rangel-Olivos <kristopherrangel@ucsb.edu>
2026-05-05 11:00:54 -0700

    WIP on helpRequest form, need to add test for form

--- frontend/src/main/components/HelpRequests/HelpRequestForm.jsx
+++ frontend/src/main/components/HelpRequests/HelpRequestForm.jsx
@@ -34,10 +34,6 @@ function HelpRequestForm({
     /^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d(:[0-5]\d(\.\d+)?)?$/;
   // Stryker restore Regex
 
-  // Stryker disable next-line all
-  const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
-  // Stryker restore Regex
-
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
       {initialContents && (
