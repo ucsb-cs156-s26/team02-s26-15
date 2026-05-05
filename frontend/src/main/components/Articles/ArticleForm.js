ae66fcba8bb105767514eee4a873de4d520f2e53
SteveZhaoUcsb <xuanbo@ucsb.edu>
2026-05-05 00:37:40 -0700

    xuanbo - WIP on commit form
    We need  to add the Z for the timezone on create and edit.
    We also need to remove the Z that we get from the backend when filling in the initial contents.

--- frontend/src/main/components/Articles/ArticleForm.js
+++ frontend/src/main/components/Articles/ArticleForm.js
@@ -19,6 +19,13 @@ function ArticleForm({
 
   const testIdPrefix = "ArticleForm";
 
+  // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+  // Note that even this complex regex may still need some tweaks
+
+  // Stryker disable Regex
+  const isodate_regex =
+    /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+  // Stryker restore Regex
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
       {initialContents && (
@@ -68,6 +75,72 @@ function ArticleForm({
         />
         <Form.Control.Feedback type="invalid">
           {errors.url?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="explanation">Explanation</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-explanation"}
+          id="explanation"
+          type="text"
+          isInvalid={Boolean(errors.explanation)}
+          {...register("explanation", {
+            required: "Explanation is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.explanation?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="email">Email</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-email"}
+          id="email"
+          type="text"
+          isInvalid={Boolean(errors.email)}
+          {...register("email", {
+            required: "Email is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.email?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="email">Email</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-email"}
+          id="email"
+          type="text"
+          isInvalid={Boolean(errors.email)}
+          {...register("email", {
+            required: "Email is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.email?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="dateAdded">Date Added (in UTC)</Form.Label>
+        <Form.Control
+            data-testid={testIdPrefix + "-dateAdded"}
+            id="dateAdded"
+            type="datetime-local"
+            isInvalid={Boolean(errors.dateAdded)}
+            {...register("dateAdded", {
+            required: true,
+            pattern: isodate_regex,
+            })}
+        />
+        <Form.Control.Feedback type="invalid">
+            {errors.dateAdded && "Date Added is required. "}
         </Form.Control.Feedback>
       </Form.Group>
 
