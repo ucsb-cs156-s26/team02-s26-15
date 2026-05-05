1ee5cb05e1b42819a7a288c828a7856f6e6d6809
katelyn-dang <katelyndang@ucsb.edu>
2026-05-04 23:21:24 -0700

    kd - WIP two of six fieldsfor menu item review form, plus story: still TBD finish other two fields on form, write tests

---
+++ frontend/src/main/components/MenuItemReviews/MenuItemReviewForm.jsx
@@ -0,0 +1,88 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+
+function MenuItemReviewForm({
+  initialContents,
+  submitAction,
+  buttonLabel = "Create"
+}) {
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: initialContents || {} });
+  // Stryker restore all
+
+  const navigate = useNavigate();
+
+  const testIdPrefix = "MenuItemReviewForm";
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      {initialContents && (
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="id">Id</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "-id"}
+            id="id"
+            type="text"
+            {...register("id")}
+            value={initialContents.id}
+            disabled
+          />
+        </Form.Group>
+      )}
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="itemId">ItemId</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-itemId"}
+          id="itemId"
+          type="text"
+          isInvalid={Boolean(errors.itemId)}
+          {...register("itemId", {
+            required: "ItemId is required.",
+            maxLength: {
+              value: 255,
+              message: "Max length 255 characters",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.itemId?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="reviewerEmail">ReviewerEmail</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-reviewerEmail"}
+          id="reviewerEmail"
+          type="text"
+          isInvalid={Boolean(errors.reviewerEmail)}
+          {...register("reviewerEmail", {
+            required: "ReviewerEmail is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.reviewerEmail?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+        {buttonLabel}
+      </Button>
+      <Button
+        variant="Secondary"
+        onClick={() => navigate(-1)}
+        data-testid={testIdPrefix + "-cancel"}
+      >
+        Cancel
+      </Button>
+    </Form>
+  );
+}
+
+export default MenuItemReviewForm;
