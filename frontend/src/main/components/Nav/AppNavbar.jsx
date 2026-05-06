125ba66ee97a86a2258b43e17978ae3950f60198
Kristopher Rangel-Olivos <kristopherrangel@ucsb.edu>
2026-05-05 22:18:09 -0700

    KR - fixed link

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -71,7 +71,7 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/placeholder">
+                  <Nav.Link as={Link} to="/helprequest">
                     Help Requests
                   </Nav.Link>
                 </>
