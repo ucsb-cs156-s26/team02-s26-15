843baad00bb7beb6803970b12b7f83eeb3ad71f4
Mikaela Fitz <mikaelafitz@ucsb.edu>
2026-05-05 18:45:54 -0700

    mf - ucsbdcmi fixed format issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -74,7 +74,11 @@ function App() {
       )}
       {hasRole(currentUser, "ROLE_USER") && (
         <>
-          <Route exact path="/diningcommonsmenuitem" element={<UCSBDiningCommonsMenuItemIndexPage />} />
+          <Route
+            exact
+            path="/diningcommonsmenuitem"
+            element={<UCSBDiningCommonsMenuItemIndexPage />}
+          />
         </>
       )}
       {hasRole(currentUser, "ROLE_ADMIN") && (
