73095a20b6db701b595c1b334b1a3fc2a31de0d6
Kristopher Rangel-Olivos <kristopherrangel@ucsb.edu>
2026-05-05 23:19:08 -0700

    KR - replace helpRequests creeate placeholder with real create page logic

--- frontend/src/main/pages/HelpRequests/HelpRequestCreatePage.jsx
+++ frontend/src/main/pages/HelpRequests/HelpRequestCreatePage.jsx
@@ -1,11 +1,51 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import HelpRequestForm from "main/components/HelpRequests/HelpRequestForm";
+import { Navigate } from "react-router";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function HelpRequestCreatePage() {
-  // Stryker disable all : placeholder for future implementation
+export default function HelpRequestCreatePage({ storybook = false }) {
+  const objectToAxiosParams = (helprequest) => ({
+    url: "/api/helprequests/post",
+    method: "POST",
+    params: {
+      requesterEmail: helprequest.requesterEmail,
+      teamId: helprequest.teamId,
+      tableOrBreakoutRoom: helprequest.tableOrBreakoutRoom,
+      requestTime: helprequest.requestTime,
+      explanation: helprequest.explanation,
+      solved: helprequest.solved,
+    },
+  });
+
+  const onSuccess = (helprequest) => {
+    toast(
+      `New help request Created - id: ${helprequest.id} requesterEmail: ${helprequest.requesterEmail}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/helprequests/all"], // mutation makes this key stale so that pages relying on it reload
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/helprequest" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New HelpRequest</h1>
+        <HelpRequestForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   );
