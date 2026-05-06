f595e9128539da78e60cc923f9d325f98ce219f8
katelyn-dang <katelyndang@ucsb.edu>
2026-05-05 17:59:11 -0700

    kd - testing swagger

--- frontend/src/main/pages/MenuItemReviews/MenuItemReviewsCreatePage.jsx
+++ frontend/src/main/pages/MenuItemReviews/MenuItemReviewsCreatePage.jsx
@@ -1,11 +1,47 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemReviewForm from "main/components/MenuItemReviews/MenuItemReviewForm";
+import { Navigate } from "react-router";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function MenuItemReviewsCreatePage() {
-  // Stryker disable all : placeholder for future implementation
+export default function MenuItemReviewsCreatePage({ storybook = false }) {
+  const objectToAxiosParams = (restaurant) => ({
+    url: "/api/menuItemReviews/post",
+    method: "POST",
+    params: {
+      name: menuItemReview.name,
+      description: menuItemReview.description,
+    },
+  });
+
+  const onSuccess = (restaurant) => {
+    toast(
+      `New restaurant Created - id: ${restaurant.id} name: ${restaurant.name}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/restaurants/all"], // mutation makes this key stale so that pages relying on it reload
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/restaurants" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Restaurant</h1>
+        <RestaurantForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   );
