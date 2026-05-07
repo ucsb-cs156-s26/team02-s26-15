e935d08f2d00d285c9e6a28459b8f3893d401f6a
katelyn-dang <katelyndang@ucsb.edu>
2026-05-06 21:09:08 -0700

    Fix routes after conflict resolution

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,14 @@ import UCSBOrganizationsIndexPage from "main/pages/UCSBOrganizations/UCSBOrganiz
 import UCSBOrganizationsCreatePage from "main/pages/UCSBOrganizations/UCSBOrganizationsCreatePage";
 import UCSBOrganizationsEditPage from "main/pages/UCSBOrganizations/UCSBOrganizationsEditPage";
 
+import UCSBDiningCommonsMenuItemIndexPage from "main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemIndexPage";
+import UCSBDiningCommonsMenuItemCreatePage from "main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage";
+import UCSBDiningCommonsMenuItemEditPage from "main/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemEditPage";
+
+import ArticlesIndexPage from "main/pages/Articles/ArticlesIndexPage";
+import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
+import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
+
 import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
@@ -43,15 +51,26 @@ function App() {
       {hasRole(currentUser, "ROLE_USER") && (
         <>
           <Route path="/ucsbdates" element={<UCSBDatesIndexPage />} />
+
           <Route path="/restaurants" element={<RestaurantIndexPage />} />
+
           <Route
             path="/menuItemReviews"
             element={<MenuItemReviewsIndexPage />}
           />
+
           <Route
             path="/ucsborganizations"
             element={<UCSBOrganizationsIndexPage />}
           />
+
+          <Route
+            path="/diningcommonsmenuitem"
+            element={<UCSBDiningCommonsMenuItemIndexPage />}
+          />
+
+          <Route path="/articles" element={<ArticlesIndexPage />} />
+
           <Route path="/placeholder" element={<PlaceholderIndexPage />} />
         </>
       )}
@@ -65,6 +84,7 @@ function App() {
             path="/restaurants/edit/:id"
             element={<RestaurantEditPage />}
           />
+
           <Route
             path="/restaurants/create"
             element={<RestaurantCreatePage />}
@@ -74,6 +94,7 @@ function App() {
             path="/menuItemReviews/edit/:id"
             element={<MenuItemReviewsEditPage />}
           />
+
           <Route
             path="/menuItemReviews/create"
             element={<MenuItemReviewsCreatePage />}
@@ -83,15 +104,37 @@ function App() {
             path="/ucsborganizations/edit/:orgCode"
             element={<UCSBOrganizationsEditPage />}
           />
+
           <Route
             path="/ucsborganizations/create"
             element={<UCSBOrganizationsCreatePage />}
           />
 
           <Route
+            path="/diningcommonsmenuitem/edit/:id"
+            element={<UCSBDiningCommonsMenuItemEditPage />}
+          />
+
+          <Route
+            path="/diningcommonsmenuitem/create"
+            element={<UCSBDiningCommonsMenuItemCreatePage />}
+          />
+
+          <Route
+            path="/articles/edit/:id"
+            element={<ArticlesEditPage />}
+          />
+
+          <Route
+            path="/articles/create"
+            element={<ArticlesCreatePage />}
+          />
+
+          <Route
             path="/placeholder/edit/:id"
             element={<PlaceholderEditPage />}
           />
+
           <Route
             path="/placeholder/create"
             element={<PlaceholderCreatePage />}
