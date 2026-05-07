1c5605016d67f795fc4da393220cb7dce4220c29
katelyn-dang <katelyndang@ucsb.edu>
2026-05-06 21:58:51 -0700

    Fix AppNavBar tests after merge conflicts

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { Link } from "react-router";
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
 import { useSystemInfo } from "main/utils/systemInfo";
 import { hasRole } from "main/utils/useCurrentUser";
+
+function AppNavbarLocalhost() {
+  return (
+    <Navbar.Text data-testid="AppNavbarLocalhost" className="me-3">
+      <span data-testid="AppNavbarLocalhost-message1">
+        Running on http://localhost:3000/ with no backend.
+      </span>
+      <br />
+      <span data-testid="AppNavbarLocalhost-message2">
+        You probably want http://localhost:8080 instead.
+      </span>
+    </Navbar.Text>
+  );
+}
 
 export default function AppNavbar({
   currentUser,
   systemInfo,
   doLogout,
-  oauthLogin,
+  oauthLogin = "/oauth2/authorization/google",
 }) {
-  const systemInfoDataFromHook = useSystemInfo();
-  const systemInfoData = systemInfo || systemInfoDataFromHook;
+  const systemInfoFromHook = useSystemInfo();
+  const systemInfoData = systemInfo || systemInfoFromHook;
+
+  const isLocalhost =
+    (window.location.hostname === "localhost" ||
+      window.location.hostname === "127.0.0.1") &&
+    window.location.port === "3000";
 
   return (
     <Navbar expand="lg" data-testid="AppNavbar">
@@ -21,9 +40,10 @@ export default function AppNavbar({
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {currentUser && currentUser.loggedIn ? (
+            {currentUser && currentUser.loggedIn && (
               <>
                 <Nav.Link as={Link} to="/ucsbdates">
                   UCSB Dates
@@ -42,9 +62,15 @@ export default function AppNavbar({
                 </Nav.Link>
 
                 {hasRole(currentUser, "ROLE_ADMIN") && (
-                  <Nav.Link as={Link} to="/admin/users">
-                    Admin
-                  </Nav.Link>
+                  <NavDropdown
+                    title="Admin"
+                    id="appnavbar-admin-dropdown"
+                    data-testid="appnavbar-admin-dropdown"
+                  >
+                    <NavDropdown.Item as={Link} to="/admin/users">
+                      Users
+                    </NavDropdown.Item>
+                  </NavDropdown>
                 )}
 
                 {systemInfoData?.springH2ConsoleEnabled && (
@@ -55,12 +81,12 @@ export default function AppNavbar({
                   <Nav.Link href="/swagger-ui/index.html">Swagger</Nav.Link>
                 )}
               </>
-            ) : (
-              <></>
             )}
           </Nav>
 
           <Nav className="ml-auto">
+            {isLocalhost && <AppNavbarLocalhost />}
+
             {currentUser && currentUser.loggedIn ? (
               <>
                 <Navbar.Text className="me-3">
